6d0172061e3ac051a4c7da7e19ddad4c23f36d0c
acapai <andria.c@eronservice.fr>
2022-04-12 00:26:49 +0200

    *Titre commit: correction schema swagger userId pour delete requête

--- src/utils/validators/deleteUser.validator.ts
+++ src/utils/validators/deleteUser.validator.ts
@@ -4,12 +4,12 @@ import Joi from "joi"
 
 export type deleteEmailProps = {
     email: string,
-    id:number,
+    userId:number,
 }
 
 export const deleteEmailSchema = Joi.object<deleteEmailProps>({
     email: Joi.string().email().lowercase().trim(true).required().example("thibault@example.com"),
-    id:Joi.number().required()
+    userId:Joi.number().required()
 });
 // const emailStringSchema = Joi.string().email().lowercase().trim(true).required().example("thibault@example.com")
 // export const emailStringSchemaSwg = j2s(emailStringSchema).swagger
